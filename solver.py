4dd096bbb52e68fc1673e0f3e479f542a8882203
Noah Davis <ndavis@my.uri.edu>
2016-10-02 17:16:09 -0400

    test2

--- solver.py
+++ solver.py
@@ -15,13 +15,41 @@ def shortest_path(start, end):
     You can use the rubik.quarter_twists move set.
     Each move can be applied using rubik.perm_apply
     """
-    print(start)
-    a = rubik.perm_apply(start, rubik.F)
-    b = rubik.perm_apply(a, rubik.Fi)
-    print(a)
 
-    print(b)
+    s = start
 
-    return
+    # print(start)
+    # a = rubik.perm_apply(start, rubik.F)
+    # b = rubik.perm_apply(a, rubik.Fi)
+    # print(a)
+    #
+    # print(b)
+
+    depth = {s:0}
+    parent = {s:None}
+    i = 1
+    visited = []
+    frontier = [s]
+    while frontier:
+        next = []
+        for u in frontier:
+            adj = []
+            adj.append(rubik.perm_apply(u, rubik.F))
+            adj.append(rubik.perm_apply(u, rubik.Fi))
+            adj.append(rubik.perm_apply(u, rubik.L))
+            adj.append(rubik.perm_apply(u, rubik.Li))
+            adj.append(rubik.perm_apply(u, rubik.U))
+            adj.append(rubik.perm_apply(u, rubik.Ui))
+            for v in adj:
+                if v not in visited:
+                    visitv()
+				if v not in depth:
+					depth[v] = i
+					parent[v] = u
+					next.append[v]
+	    frontier = next
+	    i+=1
+
+    return (whatever)
 
 shortest_path(rubik.I, rubik.I)
