6ea206ce2071a44078111773bef63d86c5c3f339
FergusonTR <fergusontr@gmail.com>
2016-10-04 20:02:50 -0400

    Getting there

--- solver.py
+++ solver.py
@@ -16,50 +16,69 @@ def shortest_path(start, end):
     You can use the rubik.quarter_twists move set.
     Each move can be applied using rubik.perm_apply
     """
-    i = 0
-    """Tracking for those nodes visited"""
-    visited = [start]
-    """my search queue"""
-    queue = []
-    path =[]
-    parent = {start:None}
-    count = 0
-    queue.append(start)
-    while len(queue) > 0:
-        count +=1
+    i = 1
+    visited = [start]                                   # list of nodes visited
+    start_queue = []                                    # start queue
+    end_queue = []                                      # end queue
+    path = []                                           # used to trace path
+    start_parent = {start: None}                        # create a start dictionary
+    end_parent = {end: None}                            # create an end dictionary
+    start_queue.append(start)                           # add the start node
+    end_queue.append(end)                               # add the end node
+    twists = rubik.quarter_twists                       # list of twists
+    """Discuss the algorithm here"""
+    while len(start_queue and end_queue) > 0:           # keep looping until a queue is empty
         """remove the first item from the queue"""
-        node = queue.pop(0)
-        """Develop each of the child nodes"""
-        adj = []
-        adj.append(rubik.perm_apply(node, rubik.F))
-        adj.append(rubik.perm_apply(node, rubik.Fi))
-        adj.append(rubik.perm_apply(node, rubik.L))
-        adj.append(rubik.perm_apply(node, rubik.Li))
-        adj.append(rubik.perm_apply(node, rubik.U))
-        adj.append(rubik.perm_apply(node, rubik.Ui))
-        if node == end:
-            # path.append(node)
-            while parent[node] != None:
-                next_one = parent[node]
-                path.append(node)
-                node = next_one
-            path.reverse()
-            return path
-        for visit in adj:
-            if visit not in visited:
-                visited.append(visit)
-                queue.append(visit)
-                parent[visit] = node
+        start_node = start_queue.pop(0)                 # pop a node to explore
+        end_node = end_queue.pop(0)                     # pop a node to explore
+        for turn in twists:
+            next_start_node = rubik.perm_apply(turn,start_node)
+            next_end_node = rubik.perm_apply(turn,end_node)
+            if next_start_node not in start_parent:
+                start_parent[next_start_node] = (start_node, turn)
+                start_queue.append(next_start_node)
+            if next_end_node not in end_parent:
+                end_parent[next_end_node] = (end_node, turn)
+                end_queue.append(next_end_node)
+            if next_start_node in end_parent or next_end_node in start_parent:                      # terminating condition
+                """Unwind the paths and merge them"""
+                while end_parent[next_end_node] is not None:
+                    pair = end_parent[next_end_node]
+                    parent_position = pair[0]
+                    move = pair[1]
+                    path.append(rubik.perm_inverse(move))
+                    next_end_node = parent_position
+                next_start_node = path[0]
+                while start_parent[next_start_node] is not None:
+                    pair = start_parent[next_start_node]
+                    parent_position = pair[0]
+                    move = pair[1]
+                    path.insert(0,move)
+                    next_start_node = parent_position
+                return path                                 # return answer
+            """
+            for visit in twists:
+                next_visit = rubik.perm_apply(visit)
+                if visit not in visited:
+                    visited.append(visit)
+                    start_parent[visit] = (node,visit)
+                    queue.append(visit)
+            """
+            i += 1
 
-"""
+"""Test Run"""
 start = rubik.I
 middle = rubik.perm_apply(rubik.F, start)
 end = rubik.perm_apply(rubik.L, middle)
 ans = shortest_path(start, end)
+
+
+
 print("starting position")
 print(start)
 print ("ending position")
 print(end)
-answer =(shortest_path(start, end))
 print("answer is")
-print (answer)"""
+print (ans)
+print("Test Answer is:")
+print(rubik.F,rubik.L)
